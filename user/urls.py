d7b745cc7c9ddb2f63dfd0725639d4060a9bfff1
simseulnyang <happysseul627@gmail.com>
2023-08-03 02:25:27 +0900

    jwt 사용 url 설정

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
 from django.contrib import admin
 from user import views
+from user.views import SeasonTokenObtainPairView
 from rest_framework_simplejwt.views import (
-    TokenRefreshView, TokenObtainPairView,
+    TokenObtainPairView,
+    TokenRefreshView,
 )
 
 urlpatterns = [
     # user/
     path('', views.UserView.as_view()),
-    path('api/token/', views.UserTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('api/authonly/', views.OnlyAuthenticatedUserView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/season/token/', SeasonTokenObtainPairView.as_view(), name='season_token'),
 ]
