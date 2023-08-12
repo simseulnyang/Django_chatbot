05f03b0903fcaa76f6be65ed9634e057c70ea9d3
simseulnyang <happysseul627@gmail.com>
2023-08-12 23:47:36 +0900

    access token lifetime 변경

--- chatbotproject/settings.py
+++ chatbotproject/settings.py
@@ -167,7 +167,7 @@ AUTH_USER_MODEL = 'user.User'
 
 # simple jwt 옵션
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
