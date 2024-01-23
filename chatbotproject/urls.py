c06cd4e35f0241e9f1ed7a1f4799052a91f9b734
Seulgi Lee <123173170+simseulnyang@users.noreply.github.com>
2024-01-23 23:17:30 +0900

    gh-3 : 기존 프로젝트 설정 수정하기 (#8)
    * :sparkles:Feat : gitmessage file 생성 #3
    
    * :sparkles:Feat : requriements.txt 작성 #3
    
    * :sparkles:Feat : asgi 작성 #3
    
    * :sparkles:Feat : swagger 관련 url패턴 추가 #3
    
    * :recycle:Refactor : 프로젝트 기본 설정 수정 #3
    
    - 불필요한 주석 삭제
    - env file 생성 및 secret_key, openai_api_key 저장
    - 필요한 앱 설치 및 INSTALLED_APPS에 추가
    - ASGI 설정
    - Swagger setting
    - simple_jwt setting
    - 디버깅 시 logging하도록 설정

--- chatbotproject/urls.py
+++ chatbotproject/urls.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django channels & chatGPT를 통한 채팅서비스",
+        default_version='v2',
+        description="simseulnyang의 개인프로젝트",
+        contact=openapi.Contact(email="happysseul627@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('chat/', include('chat.urls')),
+    # Swagger
+    path("swagger/docs/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
