5e64eee82f055889c75f188db66f6e92587077d5
simseulnyang <happysseul627@gmail.com>
2024-01-25 22:08:37 +0900

    :recycle:Refactor : 회원가입, 로그인 serializer code refactoring #1
    - serializer를 통해 유효성 검사 및 토큰 생성기능하도록 code refactoring

--- user/serializers.py
+++ user/serializers.py
@@ -1,27 +1,68 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
-from .models import User, Profile
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from user.models import User, Profile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'name']
+
 
 class UserSignupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['email', 'name', 'password']
+        extra_kwargs = {"password": {"write_only": True, "required": True}}
 
-    def create(self, *args, **kwargs):
-        user = super().create(*args, **kwargs)
-        password = user.password
-        user.set_password(password)
-        user.save()
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            name=validated_data["name"],
+            passworld=validated_data["password"]
+        )
         return user
     
-    def update(self, *args, **kwargs):
-        user = super().update(*args, **kwargs)
-        password = user.password
-        user.set_password(password)
-        user.save()
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(requried=True, write_only=True)
+    token = serializers.SerializerMethodField(read_only=True)
+
+    def validate(self, data):
+        user = authenticate(email=data["email"], password=data["password"])
+        
+        if user is None:
+            raise serializers.ValidationError("email or Password is Incorrect!")
+        
+        return data
+    
+    def get_token(self, user):
+        if user is not None:
+            refresh = TokenObtainPairSerializer.get_token(user)
+            refresh["email"] = user.email
+            data = {
+                "token" : {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                },
+                "message": "Login Success!"
+                
+            }
+            return data
+        return None
+    
+    def create(self, validated_data):
+        user = authenticate(email=validated_data["email"], password=validated_data["password"])
+        
+        if user is not None:
+            user.is_active = True
+            user.save()
         return user
-
-
 
 class ProfileSerializer(serializers.ModelSerializer):
 
@@ -30,10 +71,3 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ['profile_img', 'about_me']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    
-    profile = ProfileSerializer()
-
-    class Meta:
-        model = User
-        fields = ['email', 'nickname', 'date_joined', 'profile']
