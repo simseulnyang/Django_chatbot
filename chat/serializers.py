020e9a2167a25ac095c3a1d1c2166744bf11781d
simseulnyang <happysseul627@gmail.com>
2024-01-30 22:34:03 +0900

    :recycle:Refactor : chatRoleplaying을 위한 serializer, view, url pattern에 대한 코드 리팩토링 #6

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
-from chat.models import Conversation
+from chat.models import ChatRolePlaying, Conversation
 
-class ConversationSerializer(serializers.ModelSerializer):
+class ChatRolePlayingCreateSerializer(serializers.ModelSerializer):
 
+    class Meta:
+        model = ChatRolePlaying
+        fields = ['user', 'language', 'level', 'situation', 'my_role', 'gpt_role']
+
+
+class ChatRolePlayingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ChatRolePlaying
+        fields = '__all__'
+
+
+class ChatRolePlayingConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ['prompt', 'response']
-        extra_kwargs = {
-            'response':{"read_only":True}
-        }
