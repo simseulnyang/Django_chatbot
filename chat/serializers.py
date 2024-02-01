e40827ba80b41ff258bd5ac5193940757b70a5c8
simseulnyang <happysseul627@gmail.com>
2024-02-01 22:53:32 +0900

    :recycle:Refactor : chatplayingroom을 생성하기 위한 serializer 코드 리팩토링 #6
    - PrimaryKeyRelatedField를 사용하여 user 정보 받기
    - room 생성을 위한 데이터 유효성 검사 실시

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
+from rest_framework.serializers import PrimaryKeyRelatedField
+
 from chat.models import ChatRolePlaying, Conversation
+from user.models import User
 
 class ChatRolePlayingCreateSerializer(serializers.ModelSerializer):
+    user= PrimaryKeyRelatedField(queryset=User.objects.all())
 
     class Meta:
         model = ChatRolePlaying
         fields = ['user', 'language', 'level', 'situation', 'my_role', 'gpt_role']
+
+    def create(self, validated_data):
+        chatplayingroom = super().create(validated_data)
+        return chatplayingroom
 
 
 class ChatRolePlayingSerializer(serializers.ModelSerializer):
