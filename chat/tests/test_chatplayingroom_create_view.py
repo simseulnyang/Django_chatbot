64289363f1b2959ee0f82384c86e4b33fbad49d1
simseulnyang <happysseul627@gmail.com>
2024-02-04 23:04:28 +0900

    ✅ Test : chatplayingroom create 관련 test code 작성 #6

---
+++ chat/tests/test_chatplayingroom_create_view.py
@@ -0,0 +1,89 @@
+import json
+
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework import status
+
+User = get_user_model()
+
+LOGIN_USER_URL = reverse("login")
+CHATPLAYINGROOM_CREATE_URL = reverse("chatroleplaying-create")
+
+
+class ChatPlayingroomCreateViewTest(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # 테스트 사용자 생성
+        cls.user1 = User.objects.create_user(email="testuser01@email.com", password="testpassword", name="testuser01")
+
+        cls.user1_data={
+            "email": "testuser01@email.com",
+            "password": "testpassword"
+        }
+
+    def setUp(self):
+        res1 = self.client.post(LOGIN_USER_URL, data=json.dumps(self.user1_data), content_type="application/json",)
+        self.access_token1 = res1.data["token"]["access"]
+
+    def test_post_chatplayingroom_create_sucess(self):
+        payload = {
+            "user": 1,
+            "language": "ko-kr",
+            "level": "1",
+            "situation": "빽다방에서 커피 주문하기3",
+            "my_role": "손님",
+            "gpt_role": "빽다방 직원",
+        }
+        response = self.client.post(
+            CHATPLAYINGROOM_CREATE_URL,
+            payload,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token1}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_chatplayingroom_create_fail_unauthenticated(self):
+        payload = {
+            "user": 2,
+            "language": "ko-kr",
+            "level": "1",
+            "situation": "빽다방에서 커피 주문하기3",
+            "my_role": "손님",
+            "gpt_role": "빽다방 직원",
+        }
+        response = self.client.post(
+            CHATPLAYINGROOM_CREATE_URL,
+            payload
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_post_chatplayingroom_create_fail_invalid_user_pk(self):
+        payload = {
+            "user": 2,
+            "language": "ko-kr",
+            "level": "1",
+            "situation": "빽다방에서 커피 주문하기3",
+            "my_role": "손님",
+            "gpt_role": "빽다방 직원",
+        }
+        response = self.client.post(
+            CHATPLAYINGROOM_CREATE_URL,
+            payload,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token1}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_chatplayingroom_create_fail_not_enough_params(self):
+        payload = {
+            "situation": "빽다방에서 커피 주문하기3",
+            "my_role": "손님",
+            "gpt_role": "빽다방 직원",
+        }
+        response = self.client.post(
+            CHATPLAYINGROOM_CREATE_URL,
+            payload,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token1}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
