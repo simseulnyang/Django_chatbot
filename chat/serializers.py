7500d241cd902795a225b545c99545a3c1c44599
Seulgi Lee <123173170+simseulnyang@users.noreply.github.com>
2024-02-07 15:38:56 +0900

    gh_10: 상황, 역할 등 한국어로 작성한 내용 영어 번역 API 기능 추가 (#12)
    * ✅ Test : chatplayingroom_translation 관련 test code 작성 #10
    
    * ✨ Feat : 초기 프롬프트 작성을 위해 situation, my_role, gpt_role 번역 translation 코드 작성 #10
    
    * :sparkles: Feat : translation 관련 serializer, view, url pattern 작성 #10
    
    - 번역과 관련된 serializer인 ChatRolePlayingTranslateSerializer 작성
    - 번역 요청에 대한 응답을 처리하기 위한 ChatplayingroomTranslatingAPIView 뷰 작성
    - 번역 기능을 요청할 수 있는 API 엔드포인트에 대한 URL 패턴 설정

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.serializers import PrimaryKeyRelatedField
 
+from chat.translators import google_translate
 from chat.models import ChatRolePlaying, Conversation
 from user.models import User
 
@@ -27,3 +28,50 @@ class ChatRolePlayingConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ['prompt', 'response']
+
+
+class ChatRolePlayingTranslateSerializer(serializers.ModelSerializer):
+    situation_en = serializers.SerializerMethodField()
+    my_role_en = serializers.SerializerMethodField()
+    gpt_role_en = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ChatRolePlaying
+        fields = [
+            "language",
+            "level",
+            "situation",
+            "situation_en",
+            "my_role",
+            "my_role_en",
+            "gpt_role",
+            "gpt_role_en",
+        ]
+
+    def get_situation_en(self, obj):
+        if obj.situation_en:
+            return obj.situation_en
+        if obj.situation:
+            return self._translate(obj.situation)
+        return None
+
+    def get_my_role_en(self, obj):
+        if obj.my_role_en:
+            return obj.my_role_en
+        if obj.my_role:
+            return self._translate(obj.my_role)
+        return None
+
+    def get_gpt_role_en(self, obj):
+        if obj.gpt_role_en:
+            return obj.gpt_role_en
+        if obj.gpt_role:
+            return self._translate(obj.gpt_role)
+        return None
+
+    @staticmethod
+    def _translate(origin_text: str) -> str:
+        translated = google_translate(origin_text, "auto", "en")
+        if not translated:
+            raise serializers.ValidationError("구글 번역을 할 수 없습니다.")
+        return translated
--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from chat.views import ChatRolePlayingCreateAPIView, ChatRolePlayingListAPIView
+from chat.views import ChatRolePlayingCreateAPIView, ChatRolePlayingListAPIView, ChatplayingroomTranslatingAPIView
 
 
 urlpatterns = [
 	path("", ChatRolePlayingListAPIView.as_view(), name="chatroleplaying-list"),
     path("new/", ChatRolePlayingCreateAPIView.as_view(), name="chatroleplaying-create"),
+    path("translation/", ChatplayingroomTranslatingAPIView.as_view(), name="chatplayingTranslation"),
 ]
